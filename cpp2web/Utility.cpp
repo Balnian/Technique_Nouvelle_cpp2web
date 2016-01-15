338eab950138373eb319d95c89034cec44817288
Francis <super_f_qc@hotmail.com>
2016-01-15 01:32:31 -0500

    ajout au source

---
+++ cpp2web/Utility.cpp
@@ -0,0 +1,31 @@
+﻿#include "Utility.h"
+#include <algorithm>
+#include <locale>
+
+
+
+
+
+Utility::~Utility()
+{
+}
+
+vector<string> Utility::extractWords(string s)
+{
+	vector<string> str;
+
+	auto debut = begin(s), fin = end(s);
+	//S�pare les string et les espaces
+	while (debut != fin)
+	{
+		auto t = find_if(debut, fin, [](char c) {return !isspace(c, locale{ "" }); });
+		//if(debut!=t)
+
+		auto t2 = find_if(t, fin, [](char c) {return isspace(c, locale{ "" }); });
+
+		str.emplace_back(string(t, t2));
+		debut = t2;
+	}
+
+	return str;
+}
---
+++ cpp2web/cpp2web.cpp
@@ -0,0 +1,66 @@
+#include "cpp2web.h"
+#include <sstream>
+#include <iostream>
+#include "Utility.h"
+
+
+
+cpp2web::cpp2web()
+{
+	switchAction["-stats"] = [&](vector<string> files) -> void { this->stats(files); };
+
+	switchAction["-couleur"] = [&](vector<string> files) -> void { this->couleur(files); };
+}
+
+cpp2web::cpp2web(vector<string> args) :cpp2web()
+{
+	//check if request was well formed
+	if (end(args) != find_if(begin(args), end(args), [](string s) {
+		return Utility::str_begin_not_with(s, "-")
+			&& Utility::str_begin_not_with(s, "/")
+			&& !s.size() > 1
+			&& Utility::str_end_not_with(s, ".h")
+			&& Utility::str_end_not_with(s, ".cpp");
+	}))
+	{
+		cout << "Paramêtre invalide!!!" << endl;
+	}
+	else
+	{
+		vector<string> files{ args.size() }, actions{ args.size() };
+		//split switch from files
+		auto point = partition_copy(begin(args), end(args), begin(files), begin(actions), [](string s) {return Utility::str_begin_not_with(s, "-") && Utility::str_begin_not_with(s, "/"); });
+
+		//if there is no switch(s) or no file(s) print an error
+		if (files.size() <= 0 || actions.size() <= 0)
+		{
+			cout << "Aucun fichier ou option passer en parametre" << endl;
+		}
+		else
+		{
+			for_each(begin(actions), end(actions), [&](string swtch) {
+				(*this)[swtch](files);
+			});
+		}
+	}
+	
+}
+
+
+cpp2web::~cpp2web()
+{
+}
+
+void cpp2web::stats(vector<string> files)
+{
+	for_each(begin(files), end(files), [](string s)-> void {
+		stringstream src;
+		src << ifstream{ s, ios::binary }.rdbuf();
+		cout << src.str() << endl;
+	});
+}
+
+void cpp2web::couleur(vector<string> files)
+{
+	cout << "couleur" << endl;
+}
