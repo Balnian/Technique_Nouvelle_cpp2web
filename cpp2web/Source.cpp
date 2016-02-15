8534753ebce7cab513c516397ef8cbeca7c15b36
Francis <super_f_qc@hotmail.com>
2016-02-14 21:23:46 -0500

    Fix chrono

--- cpp2web/Source.cpp
+++ cpp2web/Source.cpp
@@ -2,18 +2,25 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <chrono>
 
 #include "Utility.h"
 #include "cpp2web.h"
 
 using namespace std;
+using namespace std::chrono;
 
 
 
 int main(int argc, char * argv[])
 {
+	auto start = high_resolution_clock::now();
 	vector<string> args(argv+1, argv+ argc); 
 	
 	cpp2web inst(args);
+
+	auto end = high_resolution_clock::now();
+	milliseconds temp= duration_cast<milliseconds>( end - start);
+	cout << temp.count() << endl;
 
 }
